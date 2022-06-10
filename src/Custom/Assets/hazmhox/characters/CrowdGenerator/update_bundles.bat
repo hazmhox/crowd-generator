a07551820d69763ba24aabfeca1b2300d87065d2
hazmhox <hazmhox@gmail.com>
2022-06-10 19:33:00 +0200

    Update the folder hierarchy to be has the same way as in VAM. Added assets.

---
+++ src/Custom/Assets/hazmhox/characters/CrowdGenerator/update_bundles.bat
@@ -0,0 +1,6 @@
+IF EXIST "crowdgenerator_assets" DEL crowdgenerator_assets.assetbundle
+IF EXIST "crowdgenerator_assets" REN crowdgenerator_assets crowdgenerator_assets.assetbundle
+
+DEL CrowdGenerator.manifest
+DEL CrowdGenerator
+DEL crowdgenerator_assets.manifest
